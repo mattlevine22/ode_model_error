58485e2ccd5917b14e4c68c88ab3f7a6996aad8b
Matt Levine <mattlevine22@gmail.com>
2021-01-17 19:03:12 -0700

    add conda_env name

--- test_experiment_0.py
+++ test_experiment_0.py
@@ -127,6 +127,7 @@ def run_summary(output_dir):
             for rfd in summary_df.rfDim.unique():
                 plot_output_dir = os.path.join(output_dir, 'summary_plots_dt{dt}_tTrain{t}_rfdim{rfd}'.format(dt=dt, t=t, rfd=rfd))
                 os.makedirs(plot_output_dir, exist_ok=True)
+                pdb.set_trace()
                 summarize_eps(df=summary_df[(summary_df.dt==dt) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd)], style='usef0', hue='type', output_dir=plot_output_dir, metric_list=metric_list)
 
 
