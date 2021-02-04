6b254b0a1204d09da5328628ce799baef567fd1e
Matt Levine <mattlevine22@gmail.com>
2021-02-04 00:31:09 -0700

    hopefully finding a plotter bug

--- test_experiment_1.py
+++ test_experiment_1.py
@@ -174,28 +174,30 @@ def run_summary(output_dir):
 
 
     ## Epsilon-based summary
-    for dt in summary_df.dt.unique():
-        for t in summary_df.tTrain.unique():
-            for rfd in summary_df.rfDim.unique():
-                plot_output_dir = os.path.join(output_dir, 'summary_plots_dt{dt}_tTrain{t}_rfdim{rfd}'.format(dt=dt, t=t, rfd=rfd))
-                os.makedirs(plot_output_dir, exist_ok=True)
-                try:
-                    summarize(df=summary_df[(summary_df.stateType!='stateAndPred') & (summary_df.dt==dt) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd)], style='diff', hue='modelType', x="f0eps", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='eps_{}')
-                    summarize(df=summary_df[(summary_df.dt==dt) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd)], style='diff', hue='modelType', x="f0eps", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='eps_all_{}')
-                except:
-                    print('plot failed for:', plot_output_dir)
+    for ZY in summary_df.ZY.unique():
+        for dt in summary_df.dt.unique():
+            for t in summary_df.tTrain.unique():
+                for rfd in summary_df.rfDim.unique():
+                    plot_output_dir = os.path.join(output_dir, 'summary_plots_dt{dt}_tTrain{t}_rfdim{rfd}'.format(dt=dt, t=t, rfd=rfd))
+                    os.makedirs(plot_output_dir, exist_ok=True)
+                    try:
+                        summarize(df=summary_df[(summary_df.stateType!='stateAndPred') & (summary_df.dt==dt) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY)], style='diff', hue='modelType', x="f0eps", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='eps_{}')
+                        summarize(df=summary_df[(summary_df.dt==dt) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY)], style='diff', hue='modelType', x="f0eps", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='eps_all_{}')
+                    except:
+                        print('plot failed for:', plot_output_dir)
 
     ## DeltaT-based summary
-    for f0eps in summary_df.f0eps.unique():
-        for t in summary_df.tTrain.unique():
-            for rfd in summary_df.rfDim.unique():
-                plot_output_dir = os.path.join(output_dir, 'summary_plots_f0eps{f0eps}_tTrain{t}_rfdim{rfd}'.format(f0eps=f0eps, t=t, rfd=rfd))
-                os.makedirs(plot_output_dir, exist_ok=True)
-                try:
-                    summarize(df=summary_df[(summary_df.stateType!='stateAndPred') & (summary_df.f0eps==f0eps) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd)], style='diff', hue='modelType', x="dt", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='dt_{}')
-                    summarize(df=summary_df[(summary_df.f0eps==f0eps) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd)], style='diff', hue='modelType', x="dt", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='dt_all_{}')
-                except:
-                    print('plot failed for:', plot_output_dir)
+    for ZY in summary_df.ZY.unique():
+        for f0eps in summary_df.f0eps.unique():
+            for t in summary_df.tTrain.unique():
+                for rfd in summary_df.rfDim.unique():
+                    plot_output_dir = os.path.join(output_dir, 'summary_plots_f0eps{f0eps}_tTrain{t}_rfdim{rfd}'.format(f0eps=f0eps, t=t, rfd=rfd))
+                    os.makedirs(plot_output_dir, exist_ok=True)
+                    try:
+                        summarize(df=summary_df[(summary_df.stateType!='stateAndPred') & (summary_df.f0eps==f0eps) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY)], style='diff', hue='modelType', x="dt", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='dt_{}')
+                        summarize(df=summary_df[(summary_df.f0eps==f0eps) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY)], style='diff', hue='modelType', x="dt", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='dt_all_{}')
+                    except:
+                        print('plot failed for:', plot_output_dir)
 
 if __name__ == '__main__':
     # if FLAGS.mode=='all':
