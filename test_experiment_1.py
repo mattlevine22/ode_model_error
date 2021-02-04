e739eeabfd07d14b043a53e8b1474ad681e66b57
Matt Levine <mattlevine22@gmail.com>
2021-02-04 00:33:25 -0700

    hopefully finding a plotter bug

--- test_experiment_1.py
+++ test_experiment_1.py
@@ -181,8 +181,8 @@ def run_summary(output_dir):
                     plot_output_dir = os.path.join(output_dir, 'summary_plots_dt{dt}_tTrain{t}_rfdim{rfd}'.format(dt=dt, t=t, rfd=rfd))
                     os.makedirs(plot_output_dir, exist_ok=True)
                     try:
-                        summarize(df=summary_df[(summary_df.stateType!='stateAndPred') & (summary_df.dt==dt) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY)], style='diff', hue='modelType', x="f0eps", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='eps_{}')
-                        summarize(df=summary_df[(summary_df.dt==dt) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY)], style='diff', hue='modelType', x="f0eps", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='eps_all_{}')
+                        summarize(df=summary_df[(summary_df.stateType!='stateAndPred') & (summary_df.dt==dt) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY) & (summary_df.fidelity=='hifi')], style='diff', hue='modelType', x="f0eps", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='eps_{}')
+                        summarize(df=summary_df[(summary_df.dt==dt) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY) & (summary_df.fidelity=='hifi')], style='diff', hue='modelType', x="f0eps", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='eps_all_{}')
                     except:
                         print('plot failed for:', plot_output_dir)
 
@@ -194,8 +194,8 @@ def run_summary(output_dir):
                     plot_output_dir = os.path.join(output_dir, 'summary_plots_f0eps{f0eps}_tTrain{t}_rfdim{rfd}'.format(f0eps=f0eps, t=t, rfd=rfd))
                     os.makedirs(plot_output_dir, exist_ok=True)
                     try:
-                        summarize(df=summary_df[(summary_df.stateType!='stateAndPred') & (summary_df.f0eps==f0eps) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY)], style='diff', hue='modelType', x="dt", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='dt_{}')
-                        summarize(df=summary_df[(summary_df.f0eps==f0eps) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY)], style='diff', hue='modelType', x="dt", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='dt_all_{}')
+                        summarize(df=summary_df[(summary_df.stateType!='stateAndPred') & (summary_df.f0eps==f0eps) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY) & (summary_df.fidelity=='hifi')], style='diff', hue='modelType', x="dt", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='dt_{}')
+                        summarize(df=summary_df[(summary_df.f0eps==f0eps) & (summary_df.tTrain==t) & (summary_df.rfDim==rfd) & (summary_df.ZY==ZY) & (summary_df.fidelity=='hifi')], style='diff', hue='modelType', x="dt", output_dir=plot_output_dir, metric_list=metric_list, fname_shape='dt_all_{}')
                     except:
                         print('plot failed for:', plot_output_dir)
 
