a4e2943db195bc450b6be8dd44b93bc77ea44d2b
mattlevine22 <mattlevine22@gmail.com>
2021-02-25 13:02:22 -0700

    set redo=True to resubmit finished jobs

--- T_run.py
+++ T_run.py
@@ -66,7 +66,8 @@ def main(cmd_py, output_dir, cmd_job, datagen, conda_env, **kwargs):
         prioritized_job_sender(all_job_fnames,
                                 bash_command=cmd_job,
                                 list_of_priorities=lop,
-                                do_all=True)
+                                do_all=True,
+                                noredo=False)
 
     run_summary(output_dir)
 
