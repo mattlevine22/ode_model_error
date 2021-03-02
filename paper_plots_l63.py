3fa91c72e5e01a05c3e0969561aa569e4cc4d950
mattlevine22 <mattlevine22@gmail.com>
2021-03-01 23:03:48 -0700

    updated plotter

--- paper_plots_l63.py
+++ paper_plots_l63.py
@@ -18,7 +18,7 @@ EVAL_DICT_ALL = {'t_valid_005': 'Validity Time', 'kl_mean': 'KL-Divergence', 'ac
 EVAL_DICT_SHORT = {'t_valid_005': 'Validity Time'}
 
 ####### EPSILON EXPERIMENT
-def plot_epsilon(base_dir, plot_dir, eval_dict=EVAL_DICT_SHORT):
+def plot_epsilon(base_dir, plot_dir, eval_dict=EVAL_DICT_ALL):
     output_dir = os.path.join(base_dir, 'l63eps_v5_eps_validateReg')
 
     full_summary_df_name = os.path.join(output_dir, 'full_summary_df.pickle')
@@ -51,10 +51,34 @@ def plot_epsilon(base_dir, plot_dir, eval_dict=EVAL_DICT_SHORT):
         fig_path = os.path.join(plot_dir, 'eps_quality_{}'.format(key))
         new_summary(df=paper_df, fig_path=fig_path, x="f0eps", y=key,
                     xlabel='$\epsilon$',
+                    estimator=np.mean,
+                    ci='sd',
                     ylabel=eval_dict[key],
                     title='Effects of model error on hybrid learning',
                     legloc='upper right',
                     figsize=(12,10))
+
+
+    paper_df = summary_df[(summary_df.rhsname.isin(papername_list)) &
+                        (summary_df.stateType=='stateAndPred') &
+                        (summary_df.fidelity==fid) &
+                        (summary_df.dt==dt) &
+                        # (summary_df.rfDim==rfDim) &
+                        (summary_df.tTrain==tTrain) &
+                        (summary_df.eval_pickle_fname.str.contains("test_eval.pickle"))]
+
+
+    for key in eval_dict:
+        fig_path = os.path.join(plot_dir, 'eps_quality_stateAndPred_{}'.format(key))
+        new_summary(df=paper_df, fig_path=fig_path, x="f0eps", y=key,
+                    xlabel='$\epsilon$',
+                    estimator=np.mean,
+                    ci='sd',
+                    ylabel=eval_dict[key],
+                    title='Effects of model error on hybrid learning',
+                    legloc='upper right',
+                    figsize=(12,10))
+
 
 
 
@@ -92,6 +116,8 @@ def plot_dt(base_dir, plot_dir, eval_dict=EVAL_DICT_ALL):
         new_summary(df=paper_df, fig_path=fig_path, x="dt", y=key,
                     xlabel='$\Delta t$',
                     ylabel=eval_dict[key],
+                    estimator=np.mean,
+                    ci='sd',
                     title='Effects of sampling rate on learning methods',
                     legloc='upper right',
                     figsize=(12,10))
@@ -133,6 +159,8 @@ def plot_T(base_dir, plot_dir, eval_dict=EVAL_DICT_SHORT):
         new_summary(df=paper_df, fig_path=fig_path, x="tTrain", y=key,
                     xlabel='Length of training trajectory (T)',
                     ylabel=eval_dict[key],
+                    estimator=np.mean,
+                    ci='sd',
                     title='Data quantity needed by hybrid learning methods',
                     legloc='best',
                     figsize=(12,10))
@@ -157,8 +185,10 @@ def plot_rfDim(base_dir, plot_dir, eval_dict=EVAL_DICT_SHORT):
     papername_list = ['rhs w/ diff=Spline, costInt=datagrid', 'Psi']
 
     fid='hifi'
-    f0eps=0.2
-    dt=0.01
+    f0eps=3.0
+    dt=0.001
+    # f0eps=0.2
+    # dt=0.01
     tTrain=100
     paper_df = summary_df[(summary_df.rhsname.isin(papername_list)) &
                         (summary_df.stateType!='stateAndPred') &
@@ -175,6 +205,8 @@ def plot_rfDim(base_dir, plot_dir, eval_dict=EVAL_DICT_SHORT):
         new_summary(df=paper_df, fig_path=fig_path, x="rfDim", y=key,
                     xlabel='$D_r$',
                     ylabel=eval_dict[key],
+                    estimator=np.mean,
+                    ci='sd',
                     title='Model complexity needed for hybrid learning',
                     legloc='best',
                     figsize=(12,10))
